87e4c2ad61a580a39f311bd0674c3d20e0a18ce6
Матвей Титов <drugbeast@MacBook-Air-Matvej.local>
2024-10-07 22:38:46 +0300

    feat: implement hash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import crypto from "crypto";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash("sha256");
+  const rs = fs.createReadStream(
+    dirname(__filename) + "/files/fileToCalculateHashFor.txt"
+  );
+  rs.on('readable', () => {
+    const data = rs.read();
+    if (data)
+      hash.update(data);
+    else {
+      console.log(`${hash.digest('hex')}`);
+    }
+  });
 };
 
 await calculateHash();
