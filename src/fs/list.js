878fa17cfe17e59f8fe709b5486e8c78b88751c0
Матвей Титов <drugbeast@MacBook-Air-Matvej.local>
2024-10-07 21:57:53 +0300

    feat: implement list.js func

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
 const list = async () => {
-    // Write your code here 
+  fs.readdir(dirname(__filename) + "/files", (err, files) => {
+    if (err) {
+        throw new Error('FS operation failed')
+    }
+    files.forEach((file) => console.log(file));
+  });
 };
 
 await list();
