be147d0f89b692fec552906dd7ef34b32e48d7f4
Матвей Титов <drugbeast@MacBook-Air-Matvej.local>
2024-10-07 22:02:25 +0300

    feat: implement read.js func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+    fs.readFile(dirname(__filename) + '/files/fileToRead1.txt', 'utf-8', (err, data) => {
+        if (err) throw new Error('FS operation failed')
+        console.log(data)
+    })
 };
 
 await read();
