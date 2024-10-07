6deb315f731329483014c6ba4dee094085aff243
Матвей Титов <drugbeast@MacBook-Air-Matvej.local>
2024-10-07 21:16:39 +0300

    feat: implement copy.js func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
 const copy = async () => {
-    // Write your code here 
+  fs.stat(dirname(__filename) + '/files', (err) => {
+    if (err == null) {
+        fs.stat(dirname(__filename) + '/files_copy', (err) => {
+            if (err == null) {
+                throw new Error('FS operation failed')
+            }
+            else if (err.code == 'ENOENT') {
+                fs.cp(dirname(__filename) + '/files', dirname(__filename) + '/files_copy', {recursive: true}, () => {})
+            }
+        })
+    }
+    else if (err.code == 'ENOENT') {
+        throw new Error('FS operation failed')
+    }
+  });
 };
 
 await copy();
